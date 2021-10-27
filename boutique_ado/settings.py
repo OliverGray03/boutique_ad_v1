9869f7db9cd6a420eabeb345fd915880d88cdf4f
OliverGray03 <Oliver.gray90@outlook.com>
2021-10-27 16:23:55 +0000

    added sizes to products

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -153,3 +153,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
